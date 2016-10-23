3705e5fffd8ff281f96e53fd8454b427a28e833d
TrippleCCC <chukwurahnonso@gmail.com>
2016-10-23 01:47:03 -0500

    added CASPRG inerface

---
+++ CASPRGInterface.java
@@ -0,0 +1,33 @@
+
+
+public class CASPRGInterface {
+	
+	
+	public static void main(String[] args)
+	{
+		MainWindow fw = new MainWindow();
+		fw.setVisible(true);
+		
+		/**
+		 * These variables will contain the the answers to the questions
+		 * \/ \/ \/ \/ \/ \/ \/
+		 */
+		String[] answers;
+		answers = new String[12];
+		answers[0] = "";
+		answers[1] = "";
+		answers[2] = "";
+		answers[3] = "";
+		answers[4] = "";
+		answers[5] = "";
+		answers[6] = "";
+		answers[7] = "";
+		answers[8] = "";
+		answers[9] = "";
+		answers[10] = "";
+		answers[11] = "";
+		answers[12] = "";
+		
+	}
+
+}
